ab29bde7c004aba3cb7e1fed89c494b57d0c72d4
RafaDSan <rafaeldss12@gmail.com>
2024-08-13 15:04:10 -0300

    feat: Updated resolver smart contract

--- src/resolver/Resolver.sol
+++ src/resolver/Resolver.sol
@@ -2,155 +2,57 @@
 
 pragma solidity ^0.8.4;
 
-import { AccessDenied, InvalidEAS, InvalidLength, uncheckedInc } from "./../Common.sol";
 import { IEAS, Attestation } from "../interfaces/IEAS.sol";
-import { Semver } from "./../Semver.sol";
-import { IResolver } from "./../interfaces/IResolver.sol";
+import { IResolver } from "../interfaces/IResolver.sol";
+import { AccessControl } from "@openzeppelin/contracts/access/AccessControl.sol";
+import { AccessDenied, InvalidEAS, InvalidLength, uncheckedInc, EMPTY_UID, NO_EXPIRATION_TIME } from "../Common.sol";
 
-/// @title SchemaResolver
-/// @notice The base schema resolver contract.
-abstract contract Resolver is IResolver, Semver {
-    error InsufficientValue();
-    error NotPayable();
+error InvalidExpiration();
+error InvalidAttestationData();
 
-    // The global EAS contract.
-    IEAS internal immutable _eas;
+/// @author Blockful | 0xneves
+/// @notice KarmaGap Resolver contract for Ethereum Attestation Service.
+contract Resolver is IResolver, AccessControl {
+  // The global EAS contract.
+  IEAS internal immutable _eas;
 
-    /// @dev Creates a new resolver.
-    /// @param eas The address of the global EAS contract.
-    constructor(IEAS eas) Semver(1, 3, 0) {
-        if (address(eas) == address(0)) {
-            revert InvalidEAS();
-        }
+  /// @dev Creates a new resolver.
+  /// @param eas The address of the global EAS contract.
+  constructor(IEAS eas) {
+    if (address(eas) == address(0)) revert InvalidEAS();
+    _eas = eas;
+  }
 
-        _eas = eas;
+  /// @dev Ensures that only the EAS contract can make this call.
+  modifier onlyEAS() {
+    if (msg.sender != address(_eas)) revert AccessDenied();
+    _;
+  }
+
+  /// @inheritdoc IResolver
+  function isPayable() public pure virtual returns (bool) {
+    return false;
+  }
+
+  /// @inheritdoc IResolver
+  function attest(Attestation calldata attestation) external payable onlyEAS returns (bool) {
+    // Prohibits the attestation expiration to be finite
+    if (attestation.expirationTime != NO_EXPIRATION_TIME) revert InvalidExpiration();
+    string memory attestationData = abi.decode(attestation.data, (string));
+    if (
+      keccak256(abi.encodePacked(attestationData)) !=
+      keccak256(abi.encodePacked("ValidAttestation"))
+    ) {
+      revert InvalidAttestationData();
     }
+    return true;
+  }
 
-    /// @dev Ensures that only the EAS contract can make this call.
-    modifier onlyEAS() {
-        _onlyEAS();
+  /// @inheritdoc IResolver
+  function revoke(Attestation calldata attestation) external payable onlyEAS returns (bool) {
+    // Schema to revoke managers
+    // TODO: Write something simple to be used in the resolver.t.sol
 
-        _;
-    }
-
-    /// @inheritdoc IResolver
-    function isPayable() public pure virtual returns (bool) {
-        return false;
-    }
-
-    /// @dev ETH callback.
-    receive() external payable virtual {
-        if (!isPayable()) {
-            revert NotPayable();
-        }
-    }
-
-    /// @inheritdoc IResolver
-    function attest(Attestation calldata attestation) external payable onlyEAS returns (bool) {
-        return onAttest(attestation, msg.value);
-    }
-
-    /// @inheritdoc IResolver
-    function multiAttest(
-        Attestation[] calldata attestations,
-        uint256[] calldata values
-    ) external payable onlyEAS returns (bool) {
-        uint256 length = attestations.length;
-        if (length != values.length) {
-            revert InvalidLength();
-        }
-
-        // We are keeping track of the remaining ETH amount that can be sent to resolvers and will keep deducting
-        // from it to verify that there isn't any attempt to send too much ETH to resolvers. Please note that unless
-        // some ETH was stuck in the contract by accident (which shouldn't happen in normal conditions), it won't be
-        // possible to send too much ETH anyway.
-        uint256 remainingValue = msg.value;
-
-        for (uint256 i = 0; i < length; i = uncheckedInc(i)) {
-            // Ensure that the attester/revoker doesn't try to spend more than available.
-            uint256 value = values[i];
-            if (value > remainingValue) {
-                revert InsufficientValue();
-            }
-
-            // Forward the attestation to the underlying resolver and return false in case it isn't approved.
-            if (!onAttest(attestations[i], value)) {
-                return false;
-            }
-
-            unchecked {
-                // Subtract the ETH amount, that was provided to this attestation, from the global remaining ETH amount.
-                remainingValue -= value;
-            }
-        }
-
-        return true;
-    }
-
-    /// @inheritdoc IResolver
-    function revoke(Attestation calldata attestation) external payable onlyEAS returns (bool) {
-        return onRevoke(attestation, msg.value);
-    }
-
-    /// @inheritdoc IResolver
-    function multiRevoke(
-        Attestation[] calldata attestations,
-        uint256[] calldata values
-    ) external payable onlyEAS returns (bool) {
-        uint256 length = attestations.length;
-        if (length != values.length) {
-            revert InvalidLength();
-        }
-
-        // We are keeping track of the remaining ETH amount that can be sent to resolvers and will keep deducting
-        // from it to verify that there isn't any attempt to send too much ETH to resolvers. Please note that unless
-        // some ETH was stuck in the contract by accident (which shouldn't happen in normal conditions), it won't be
-        // possible to send too much ETH anyway.
-        uint256 remainingValue = msg.value;
-
-        for (uint256 i = 0; i < length; i = uncheckedInc(i)) {
-            // Ensure that the attester/revoker doesn't try to spend more than available.
-            uint256 value = values[i];
-            if (value > remainingValue) {
-                revert InsufficientValue();
-            }
-
-            // Forward the revocation to the underlying resolver and return false in case it isn't approved.
-            if (!onRevoke(attestations[i], value)) {
-                return false;
-            }
-
-            unchecked {
-                // Subtract the ETH amount, that was provided to this attestation, from the global remaining ETH amount.
-                remainingValue -= value;
-            }
-        }
-
-        return true;
-    }
-
-    /// @notice A resolver callback that should be implemented by child contracts.
-    /// @param attestation The new attestation.
-    /// @param value An explicit ETH amount that was sent to the resolver. Please note that this value is verified in
-    ///     both attest() and multiAttest() callbacks EAS-only callbacks and that in case of multi attestations, it'll
-    ///     usually hold that msg.value != value, since msg.value aggregated the sent ETH amounts for all the
-    ///     attestations in the batch.
-    /// @return Whether the attestation is valid.
-    function onAttest(Attestation calldata attestation, uint256 value) internal virtual returns (bool);
-
-    /// @notice Processes an attestation revocation and verifies if it can be revoked.
-    /// @param attestation The existing attestation to be revoked.
-    /// @param value An explicit ETH amount that was sent to the resolver. Please note that this value is verified in
-    ///     both revoke() and multiRevoke() callbacks EAS-only callbacks and that in case of multi attestations, it'll
-    ///     usually hold that msg.value != value, since msg.value aggregated the sent ETH amounts for all the
-    ///     attestations in the batch.
-    /// @return Whether the attestation can be revoked.
-    function onRevoke(Attestation calldata attestation, uint256 value) internal virtual returns (bool);
-
-    /// @dev Ensures that only the EAS contract can make this call.
-    function _onlyEAS() private view {
-        if (msg.sender != address(_eas)) {
-            revert AccessDenied();
-        }
-    }
+    return true;
+  }
 }
