1e700139a554209533489678e0b1bc485f369edb
0xneves <asguilift@hotmail.com>
2024-08-27 22:18:16 -0300

    feat: grantRegistry without projectOwner validation

--- src/GrantRegistry.sol
+++ src/GrantRegistry.sol
@@ -3,48 +3,44 @@ pragma solidity ^0.8.25;
 
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IGrantRegistry } from "./interfaces/IGrantRegistry.sol";
-import { IEASResolver } from "./interfaces/IEASResolver.sol";
 
 /// @title Grant Registry
 /// @author KarmaGap | 0xneves.eth
 /// @notice Registry of grant applications.
 /// The Grant Programs can issue and manage grants.
 contract GrantRegistry is IGrantRegistry, Ownable {
-  /// The proxy address of the Karma EAS Resolver that stored the project owner on-chain.
-  address public immutable projectResolver = 0x28BE0b0515be8BB8822aF1467A6613795E74717b;
-  /// The global EAS contract.
-  address public immutable eas = 0xbD75f629A22Dc1ceD33dDA0b68c546A1c035c458;
-
   /// Map the grant UID to the grant data
   mapping(bytes32 => Grant) private _grants;
 
   constructor() Ownable(msg.sender) {}
 
   /// @inheritdoc IGrantRegistry
-  function batchRegister(
-    bytes32[] calldata grantUIDs,
-    uint256[] calldata grantProgramUIDs,
-    uint256[] calldata statuses
-  ) external onlyOwner {
-    if (grantUIDs.length != grantProgramUIDs.length || grantUIDs.length != statuses.length)
-      revert InvalidArrayLength();
-    for (uint256 i = 0; i < grantUIDs.length; i++) {
-      register(grantUIDs[i], grantProgramUIDs[i], statuses[i]);
+  function batchRegister(Grant[] memory grants) external onlyOwner {
+    for (uint256 i = 0; i < grants.length; i++) {
+      register(
+        grants[i].grantUID,
+        grants[i].grantProgramUID,
+        grants[i].grantee,
+        grants[i].chainId,
+        grants[i].status
+      );
     }
   }
 
   /// @inheritdoc IGrantRegistry
-  function register(bytes32 grantUID, uint256 grantProgramUID, uint256 status) public onlyOwner {
+  function register(
+    bytes32 grantUID,
+    uint256 grantProgramUID,
+    address grantee,
+    uint256 chainId,
+    Status status
+  ) public onlyOwner {
     if (_grants[grantUID].grantee != address(0)) revert GrantAlreadyExists();
-
-    // checks if the grantee exists on the Project Resolver
-    IEASResolver.Attestation memory attestation = IEASResolver(eas).getAttestation(grantUID);
-    address grantee = IEASResolver(projectResolver).projectOwner(attestation.refUID);
     if (grantee == address(0)) revert InvalidGrantOwner();
 
-    _grants[grantUID] = Grant(grantee, grantProgramUID, Status(status));
+    _grants[grantUID] = Grant(grantUID, grantProgramUID, grantee, chainId, Status(status));
 
-    emit GrantRegistered(grantUID, grantProgramUID, grantee, status);
+    emit GrantRegistered(grantUID, grantProgramUID, grantee, uint256(status));
   }
 
   /// @inheritdoc IGrantRegistry
--- test/GrantRegistry.t.sol
+++ test/GrantRegistry.t.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.25;
 
 import { Test, console2 } from "forge-std/src/Test.sol";
 import { GrantRegistry } from "../src/GrantRegistry.sol";
+import { IGrantRegistry } from "../src/interfaces/IGrantRegistry.sol";
 import { IEASResolver } from "../src/interfaces/IEASResolver.sol";
 
 contract GrantRegistryTest is Test {
@@ -19,20 +20,75 @@ contract GrantRegistryTest is Test {
   function test_grant_registry() public {
     bytes32[] memory grantUIDs = new bytes32[](2);
     uint256[] memory grantProgramUIDs = new uint256[](2);
+    address[] memory grantees = new address[](2);
+    uint256[] memory chains = new uint256[](2);
     uint256[] memory statuses = new uint256[](2);
 
+    // https://gap.karmahq.xyz/project/arbitrum-thailand/grants?tab=overview
     grantUIDs[0] = 0x635c2d0642c81e3191e6eff8623ba601b7e22e832d7791712b6bc28d052ff2b5;
     grantProgramUIDs[0] = 260;
+    grantees[0] = 0x343e6C4512c9f43C4b80994897627BCd29D2a956;
+    chains[0] = 42161;
     statuses[0] = 1;
-
+    // https://gap.karmahq.xyz/project/omni-analytics-group/grants?tab=overview
     grantUIDs[1] = 0xcdaa67fc005df1417c7f9a54eee0d62ec3b44d157eba707b3a0169d4cbca9bf3;
     grantProgramUIDs[1] = 260;
+    grantees[1] = 0x41D2a18E1DdACdAbFDdADB62e9AEE67c63070b76;
+    chains[1] = 42161;
     statuses[1] = 1;
 
-    grantRegistry.register(grantUIDs[0], grantProgramUIDs[0], statuses[0]);
-    grantRegistry.register(grantUIDs[1], grantProgramUIDs[1], statuses[1]);
+    grantRegistry.register(
+      grantUIDs[0],
+      grantProgramUIDs[0],
+      grantees[0],
+      chains[0],
+      IGrantRegistry.Status(statuses[0])
+    );
+    grantRegistry.register(
+      grantUIDs[1],
+      grantProgramUIDs[1],
+      grantees[0],
+      chains[0],
+      IGrantRegistry.Status(statuses[1])
+    );
 
     assert(uint256(grantRegistry.getStatus(grantUIDs[0])) == 1);
     assert(uint256(grantRegistry.getStatus(grantUIDs[1])) == 1);
+
+    grantRegistry.update(grantUIDs[0], 2);
+    assert(uint256(grantRegistry.getStatus(grantUIDs[0])) == 2);
+
+    grantRegistry.remove(grantUIDs[0]);
+  }
+
+  function test_grant_registry_batch() public {
+    IGrantRegistry.Grant[] memory grants = new IGrantRegistry.Grant[](2);
+
+    grants[0] = IGrantRegistry.Grant(
+      0x635c2d0642c81e3191e6eff8623ba601b7e22e832d7791712b6bc28d052ff2b5,
+      260,
+      0x343e6C4512c9f43C4b80994897627BCd29D2a956,
+      42161,
+      IGrantRegistry.Status(1)
+    );
+
+    grants[1] = IGrantRegistry.Grant(
+      0xcdaa67fc005df1417c7f9a54eee0d62ec3b44d157eba707b3a0169d4cbca9bf3,
+      260,
+      0x41D2a18E1DdACdAbFDdADB62e9AEE67c63070b76,
+      42161,
+      IGrantRegistry.Status(1)
+    );
+
+    grantRegistry.batchRegister(grants);
+
+    assert(uint256(grantRegistry.getStatus(grants[0].grantUID)) == 1);
+    assert(uint256(grantRegistry.getStatus(grants[1].grantUID)) == 1);
+
+    grantRegistry.update(grants[0].grantUID, 2);
+    assert(uint256(grantRegistry.getStatus(grants[0].grantUID)) == 2);
+
+    grantRegistry.remove(grants[0].grantUID);
+    grantRegistry.remove(grants[1].grantUID);
   }
 }
